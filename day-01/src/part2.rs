c74e1ce3b243993e5c4f44b0262d4431007680ec
mrkiffie <mrkiffie@gmail.com>
2025-11-21 22:24:38 +0200

    Refactor tracking of visited intersections

--- day-01/src/part2.rs
+++ day-01/src/part2.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 const INPUT: &[u8] = include_bytes!("./input.txt");
 
 pub fn main() {
@@ -36,10 +34,162 @@ impl Direction {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Aabb {
+    min: Vec2,
+    max: Vec2,
+    direction: Direction,
+}
+
+impl Aabb {
+    fn new(from: Vec2, to: Vec2) -> Self {
+        match (from.x.cmp(&to.x), from.y.cmp(&to.y)) {
+            (std::cmp::Ordering::Less, std::cmp::Ordering::Equal) => Self {
+                min: from,
+                max: to,
+                direction: Direction::E,
+            },
+            (std::cmp::Ordering::Equal, std::cmp::Ordering::Less | std::cmp::Ordering::Equal) => {
+                Self {
+                    min: from,
+                    max: to,
+                    direction: Direction::N,
+                }
+            }
+            (std::cmp::Ordering::Equal, std::cmp::Ordering::Greater) => Self {
+                min: to,
+                max: from,
+                direction: Direction::S,
+            },
+            (std::cmp::Ordering::Greater, std::cmp::Ordering::Equal) => Self {
+                min: to,
+                max: from,
+                direction: Direction::W,
+            },
+            _ => {
+                unimplemented!();
+            }
+        }
+    }
+
+    fn intersects(&self, other: &Self) -> bool {
+        let x_overlaps = self.min.x <= other.max.x && self.max.x >= other.min.x;
+        let y_overlaps = self.min.y <= other.max.y && self.max.y >= other.min.y;
+        x_overlaps && y_overlaps
+    }
+}
+
+#[derive(Debug)]
+struct Visited {
+    visited: Vec<Vec2>,
+}
+
+impl Visited {
+    fn with_capacity(capacity: usize) -> Self {
+        Self {
+            visited: Vec::with_capacity(capacity),
+        }
+    }
+
+    fn check_intersection(&mut self, next: Vec2) -> Option<Vec2> {
+        if self.visited.len() < 2 {
+            self.visited.push(next);
+            return None;
+        }
+        let previous = self.visited.last().copied().expect("length checked above");
+
+        let delta = match previous.x.cmp(&next.x) {
+            std::cmp::Ordering::Less => Vec2::X,
+            std::cmp::Ordering::Equal => {
+                if previous.y > next.y {
+                    Vec2::NEG_Y
+                } else {
+                    Vec2::Y
+                }
+            }
+            std::cmp::Ordering::Greater => Vec2::NEG_X,
+        };
+
+        let new_line = Aabb::new(previous + delta, next);
+        let Some(intersecting_line) = self
+            .visited
+            .windows(2)
+            .map(|window| Aabb::new(window[0], window[1]))
+            .find(|line| line.intersects(&new_line))
+        else {
+            self.visited.push(next);
+            return None;
+        };
+
+        // find exact intersection
+        let delta = match new_line.direction {
+            Direction::N | Direction::E => 1,
+            Direction::S | Direction::W => -1,
+        };
+
+        match new_line.direction {
+            Direction::N | Direction::S => {
+                // x is locked
+                let mut y = previous.y;
+                let x = previous.x;
+
+                while y != next.y {
+                    let point = Vec2 { x, y };
+                    if intersecting_line.intersects(&Aabb::new(point, point)) {
+                        return Some(point);
+                    }
+                    y += delta;
+                }
+
+                if intersecting_line.intersects(&Aabb::new(next, next)) {
+                    return Some(next);
+                }
+                None
+            }
+            Direction::E | Direction::W => {
+                // y is locked
+                let y = previous.y;
+                let mut x = previous.x;
+
+                while x != next.x {
+                    let point = Vec2 { x, y };
+                    if intersecting_line.intersects(&Aabb::new(point, point)) {
+                        return Some(point);
+                    }
+                    x += delta;
+                }
+
+                if intersecting_line.intersects(&Aabb::new(next, next)) {
+                    return Some(next);
+                }
+                None
+            }
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy, Default, Hash, Eq, PartialEq)]
 struct Vec2 {
-    x: i32,
-    y: i32,
+    x: i16,
+    y: i16,
+}
+
+impl Vec2 {
+    const Y: Vec2 = Vec2 { x: 0, y: 1 };
+    const X: Vec2 = Vec2 { x: 1, y: 0 };
+    const NEG_Y: Vec2 = Vec2 { x: 0, y: -1 };
+    const NEG_X: Vec2 = Vec2 { x: -1, y: 0 };
+}
+
+impl std::ops::Add for Vec2 {
+    type Output = Vec2;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, Default)]
@@ -57,7 +207,7 @@ impl Player {
         self.direction = self.direction.turn_left();
     }
 
-    fn step(&mut self, steps: i32) {
+    fn step(&mut self, steps: i16) {
         match self.direction {
             Direction::N => self.position.y += steps,
             Direction::E => self.position.x += steps,
@@ -66,18 +216,18 @@ impl Player {
         }
     }
 
-    fn manhattan_distance(self) -> i32 {
+    fn manhattan_distance(self) -> i16 {
         self.position.x.abs() + self.position.y.abs()
     }
 }
 
-fn solve(input: &[u8]) -> i32 {
+fn solve(input: &[u8]) -> i16 {
     let mut iter = input.iter();
 
     let mut player = Player::default();
 
-    let mut visited: HashSet<Vec2> = HashSet::new();
-    visited.insert(player.position);
+    let mut visited = Visited::with_capacity(128);
+    visited.check_intersection(player.position);
 
     while let Some(c) = iter.next() {
         match c {
@@ -93,19 +243,17 @@ fn solve(input: &[u8]) -> i32 {
                 } else {
                     player.turn_left();
                 }
-                let mut steps: i32 = 0;
+                let mut steps: i16 = 0;
                 while let Some(value) = iter.next()
                     && value.is_ascii_digit()
                 {
-                    steps = steps * 10 + i32::from(*value) - 48;
+                    steps = steps * 10 + i16::from(*value) - 48;
                 }
+                player.step(steps);
 
-                for _ in 0..steps {
-                    player.step(1);
-                    if visited.contains(&player.position) {
-                        return player.manhattan_distance();
-                    }
-                    visited.insert(player.position);
+                if let Some(point) = visited.check_intersection(player.position) {
+                    player.position = point;
+                    return player.manhattan_distance();
                 }
             }
             _ => {

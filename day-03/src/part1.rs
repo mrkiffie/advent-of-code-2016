6e5e38f9a3c8e29a376477192a44f04b37fe1dd9
mrkiffie <mrkiffie@gmail.com>
2025-11-23 08:28:57 +0200

    Solve day-03 part1

--- day-03/src/part1.rs
+++ day-03/src/part1.rs
@@ -1,12 +1,28 @@
-const INPUT: &[u8] = include_bytes!("./input.txt");
+const INPUT: &str = include_str!("./input.txt");
 
 pub fn main() {
     let result = solve(INPUT);
     println!("{result}");
 }
 
-fn solve(input: &[u8]) -> i32 {
-    0
+fn solve(input: &str) -> usize {
+    input.lines().filter(parse_line).count()
+}
+
+#[inline]
+fn parse_line(line: &&str) -> bool {
+    let mut parts = line
+        .split_whitespace()
+        .filter_map(|value| value.parse::<i32>().ok());
+    match (parts.next(), parts.next(), parts.next()) {
+        (Some(a), Some(b), Some(c)) => is_triangle(a, b, c),
+        _ => false,
+    }
+}
+
+#[inline]
+fn is_triangle(a: i32, b: i32, c: i32) -> bool {
+    a + b > c && a + c > b && b + c > a
 }
 
 #[cfg(test)]
@@ -15,8 +31,14 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let result = solve(b"");
+        let result = solve("5 10 25");
         assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_2() {
+        let result = solve("5 10 6\n10 5 6\n6 5 10");
+        assert_eq!(result, 3);
     }
 }
 

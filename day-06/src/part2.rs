3fb06c4153e9646921ac8a644cbd6fc3c4440bd4
mrkiffie <mrkiffie@gmail.com>
2025-11-25 21:06:19 +0200

    Solve day-06 part2

--- day-06/src/part2.rs
+++ day-06/src/part2.rs
@@ -1,22 +1,47 @@
-const INPUT: &[u8] = include_bytes!("./input.txt");
+use std::collections::HashMap;
+
+const INPUT: &str = include_str!("./input.txt");
 
 pub fn main() {
     let result = solve(INPUT);
     println!("{result}");
 }
 
-fn solve(input: &[u8]) -> i32 {
-    0
+fn solve(input: &str) -> String {
+    let size = input
+        .find('\n')
+        .expect("input should consist of multiple lines");
+    let mut counts = vec![HashMap::new(); size];
+    for line in input.lines() {
+        for (col, char) in line.chars().enumerate() {
+            match counts.get_mut(col) {
+                Some(map) => {
+                    map.entry(char).and_modify(|entry| *entry += 1).or_insert(1);
+                }
+                None => unreachable!(),
+            }
+        }
+    }
+    let mut message = String::with_capacity(size);
+
+    for col_counts in counts {
+        if let Some((c, _count)) = col_counts.iter().max_by(|a, b| b.1.cmp(a.1)) {
+            message.push(*c);
+        }
+    }
+
+    message
 }
 
 #[cfg(test)]
 mod tests {
     use super::solve;
-
     #[test]
     fn test_1() {
-        let result = solve(b"");
-        assert_eq!(result, 0);
+        let result = solve(
+            "eedadn\ndrvtee\neandsr\nraavrd\natevrs\ntsrnev\nsdttsa\nrasrtv\nnssdts\nntnada\nsvetve\ntesnvt\nvntsnd\nvrdear\ndvrsen\nenarar",
+        );
+        assert_eq!(result, "advent");
     }
 }
 

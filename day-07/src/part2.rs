9c162e99aac7275200d917489f196caa94fc01c5
mrkiffie <mrkiffie@gmail.com>
2025-11-25 22:27:55 +0200

    Optimize day-07 part2

--- day-07/src/part2.rs
+++ day-07/src/part2.rs
@@ -7,13 +7,24 @@ pub fn main() {
     println!("{result}");
 }
 
-fn solve(input: &str) -> usize {
-    input.lines().filter(|line| check(line)).count()
+#[derive(Debug, Default)]
+struct Scanner {
+    inner: HashSet<(u8, u8)>,
+    outer: HashSet<(u8, u8)>,
 }
 
-fn check(input: &str) -> bool {
-    let mut inner = HashSet::new();
-    let mut outer = HashSet::new();
+fn solve(input: &str) -> usize {
+    input
+        .lines()
+        .scan(Scanner::default(), |scanner: &mut Scanner, line| {
+            Some(usize::from(check(scanner, line)))
+        })
+        .sum()
+}
+
+fn check(scanner: &mut Scanner, input: &str) -> bool {
+    scanner.outer.clear();
+    scanner.inner.clear();
     let mut outside = true;
     for window in input.as_bytes().windows(3) {
         if window.contains(&b'[') {
@@ -28,9 +39,9 @@ fn check(input: &str) -> bool {
             [a, b, c] => {
                 if a == c && a != b {
                     if outside {
-                        outer.insert((a, b));
+                        scanner.outer.insert((a, b));
                     } else {
-                        inner.insert((b, a));
+                        scanner.inner.insert((b, a));
                     }
                 }
             }
@@ -39,20 +50,19 @@ fn check(input: &str) -> bool {
             }
         }
     }
-
-    inner.intersection(&outer).count() > 0
+    scanner.inner.intersection(&scanner.outer).count() > 0
 }
 
 #[cfg(test)]
 mod tests {
-    use super::check;
+    use super::{Scanner, check};
 
     #[test]
     fn text_check() {
-        assert!(check("aba[bab]xyz")); // supports SSL (aba outside square brackets with corresponding bab within square brackets).
-        assert!(!check("xyx[xyx]xyx")); // does not support SSL (xyx, but no corresponding yxy).
-        assert!(check("aaa[kek]eke")); // supports SSL (eke in supernet with corresponding kek in hypernet; the aaa sequence is not related, because the interior character must be different).
-        assert!(check("zazbz[bzb]cdb")); // supports SSL (zaz has no corresponding aza, but zbz has a corresponding bzb, even though zaz and zbz overlap).
+        assert!(check(&mut Scanner::default(), "aba[bab]xyz")); // supports SSL (aba outside square brackets with corresponding bab within square brackets).
+        assert!(!check(&mut Scanner::default(), "xyx[xyx]xyx")); // does not support SSL (xyx, but no corresponding yxy).
+        assert!(check(&mut Scanner::default(), "aaa[kek]eke")); // supports SSL (eke in supernet with corresponding kek in hypernet; the aaa sequence is not related, because the interior character must be different).
+        assert!(check(&mut Scanner::default(), "zazbz[bzb]cdb")); // supports SSL (zaz has no corresponding aza, but zbz has a corresponding bzb, even though zaz and zbz overlap).
     }
 }
 

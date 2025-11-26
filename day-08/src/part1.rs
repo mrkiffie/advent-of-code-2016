cc1bddc8ecc2cdd5eba9742800adceddb8e31ac6
mrkiffie <mrkiffie@gmail.com>
2025-11-26 22:03:26 +0200

    Solve day-08 part1

--- day-08/src/part1.rs
+++ day-08/src/part1.rs
@@ -1,22 +1,73 @@
-const INPUT: &[u8] = include_bytes!("./input.txt");
+const INPUT: &str = include_str!("./input.txt");
 
 pub fn main() {
     let result = solve(INPUT);
     println!("{result}");
 }
 
-fn solve(input: &[u8]) -> i32 {
-    0
+#[derive(Debug, Clone, Copy)]
+struct Vec2 {
+    x: u8,
+    y: u8,
+}
+
+const ROWS: u8 = 6;
+const COLS: u8 = 50;
+fn solve(input: &str) -> usize {
+    let pixels = parse_pixels(input);
+    pixels.len()
+}
+
+fn parse_pixels(input: &str) -> Vec<Vec2> {
+    let mut pixels = vec![];
+    input
+        .lines()
+        .fold(&mut pixels, |pixels: &mut Vec<Vec2>, line: &str| {
+            if let Some(instruction) = line.strip_prefix("rotate column x=")
+                && let Some((x, delta)) = instruction.split_once(" by ")
+            {
+                let x = x.parse::<u8>().unwrap();
+                let delta = delta.parse::<u8>().unwrap();
+                for pixel in pixels.iter_mut() {
+                    if pixel.x == x {
+                        pixel.y = (pixel.y + delta) % ROWS;
+                    }
+                }
+            } else if let Some(instruction) = line.strip_prefix("rotate row y=")
+                && let Some((y, delta)) = instruction.split_once(" by ")
+            {
+                let y = y.parse::<u8>().unwrap();
+                let delta = delta.parse::<u8>().unwrap();
+                for pixel in pixels.iter_mut() {
+                    if pixel.y == y {
+                        pixel.x = (pixel.x + delta) % COLS;
+                    }
+                }
+            } else if let Some(instruction) = line.strip_prefix("rect ")
+                && let Some((x, y)) = instruction.split_once('x')
+            {
+                let cols = x.parse::<u8>().unwrap();
+                let rows = y.parse::<u8>().unwrap();
+                for x in 0..cols {
+                    for y in 0..rows {
+                        pixels.push(Vec2 { x, y });
+                    }
+                }
+            }
+            pixels
+        });
+    pixels
 }
 
 #[cfg(test)]
 mod tests {
-    use super::solve;
+
+    use super::{INPUT, solve};
 
     #[test]
     fn test_1() {
-        let result = solve(b"");
-        assert_eq!(result, 0);
+        let result = solve(INPUT);
+        assert_eq!(result, 128);
     }
 }
 
